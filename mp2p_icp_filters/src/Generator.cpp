434616df6fe5720c949696b8ec12403ddc035f2c
Zheng Junjie <z572@z572.online>
2025-06-13 01:57:12 +0000

    Rebase from 'rpm/jazzy/mp2p_icp'

--- mp2p_icp_filters/src/Generator.cpp
+++ mp2p_icp_filters/src/Generator.cpp
@@ -41,8 +41,7 @@ using namespace mp2p_icp_filters;
 
 Generator::Generator() : mrpt::system::COutputLogger("Generator") {}
 
-void Generator::Parameters::load_from_yaml(
-    const mrpt::containers::yaml& c, Generator& parent)
+void Generator::Parameters::load_from_yaml(const mrpt::containers::yaml& c, Generator& parent)
 {
     using namespace std::string_literals;
 
@@ -64,7 +63,10 @@ void Generator::Parameters::load_from_yaml(
         for (const auto& [k, v] : mmd)
         {
             const auto key = k.as<std::string>();
-            if (v.isNullNode()) continue;  // ignore
+            if (v.isNullNode())
+            {
+                continue;  // ignore
+            }
             if (v.isScalar())
             {
                 if (allowedTopKeys.count(key) != 0)
@@ -74,16 +76,14 @@ void Generator::Parameters::load_from_yaml(
                     trg[key] = v.as<std::string>();
                     continue;
                 }
-                else
-                {
-                    THROW_EXCEPTION_FMT(
-                        "scalar key '%s' not allowed here.", key.c_str());
-                }
+
+                THROW_EXCEPTION_FMT("scalar key '%s' not allowed here.", key.c_str());
             }
             // if should then be a map
             if (!v.isMap())
-                THROW_EXCEPTION_FMT(
-                    "key '%s' must be either a scalar or a map", key.c_str());
+            {
+                THROW_EXCEPTION_FMT("key '%s' must be either a scalar or a map", key.c_str());
+            }
 
             trg[key]     = mrpt::containers::yaml::Map();
             auto& newMap = trg.asMap().at(key).asMap();
@@ -94,17 +94,14 @@ void Generator::Parameters::load_from_yaml(
                 // Special case: handle parameterizable formulas:
                 if (val.substr(0, 3) == "$f{"s)
                 {
-                    ASSERTMSG_(
-                        val.back() == '}', "Missing '}' in '$f{' formula");
+                    ASSERTMSG_(val.back() == '}', "Missing '}' in '$f{' formula");
 
                     const auto ks = kk.as<std::string>();
 
-                    auto [it, exist] = newMap.insert({ks, .0});
-                    double& placeholder =
-                        *std::any_cast<double>(&it->second.asScalar());
+                    auto [it, exist]    = newMap.insert({ks, .0});
+                    double& placeholder = *std::any_cast<double>(&it->second.asScalar());
 
-                    parent.parseAndDeclareParameter(
-                        val.substr(3, val.size() - 4), placeholder);
+                    parent.parseAndDeclareParameter(val.substr(3, val.size() - 4), placeholder);
                 }
                 else
                 {
@@ -127,9 +124,8 @@ void Generator::initialize(const mrpt::containers::yaml& c)
     MRPT_LOG_DEBUG_STREAM("Loading these params:\n" << c);
     params_.load_from_yaml(c, *this);
 
-    process_class_names_regex_ = std::regex(params_.process_class_names_regex);
-    process_sensor_labels_regex_ =
-        std::regex(params_.process_sensor_labels_regex);
+    process_class_names_regex_   = std::regex(params_.process_class_names_regex);
+    process_sensor_labels_regex_ = std::regex(params_.process_sensor_labels_regex);
 
     initialized_ = true;
     MRPT_END
@@ -143,26 +139,19 @@ bool Generator::process(
 
     Parameterizable::checkAllParametersAreRealized();
 
-    ASSERTMSG_(
-        initialized_,
-        "initialize() must be called once before using process().");
+    ASSERTMSG_(initialized_, "initialize() must be called once before using process().");
 
     const auto obsClassName = o.GetRuntimeClass()->className;
 
     MRPT_LOG_DEBUG_FMT(
-        "Processing observation type='%s' label='%s'", obsClassName,
-        o.sensorLabel.c_str());
+        "Processing observation type='%s' label='%s'", obsClassName, o.sensorLabel.c_str());
 
     // default: use point clouds:
-    if (params_.metric_map_definition_ini_file.empty() &&
-        params_.metric_map_definition.empty())
+    if (params_.metric_map_definition_ini_file.empty() && params_.metric_map_definition.empty())
     {
         return implProcessDefault(o, out, robotPose);
     }
-    else
-    {  //
-        return implProcessCustomMap(o, out, robotPose);
-    }
+    return implProcessCustomMap(o, out, robotPose);
 
     MRPT_END
 }
@@ -205,19 +194,18 @@ bool Generator::filterScan3D(  //
 
 bool Generator::filterPointCloud(  //
     const mrpt::maps::CPointsMap& pc, const mrpt::poses::CPose3D& sensorPose,
-    mp2p_icp::metric_map_t&                    out,
-    const std::optional<mrpt::poses::CPose3D>& robotPose) const
+    mp2p_icp::metric_map_t& out, const std::optional<mrpt::poses::CPose3D>& robotPose) const
 {
     // Create if new: Append to existing layer, if already existed.
     mrpt::maps::CPointsMap::Ptr outPc;
-    if (auto itLy = out.layers.find(params_.target_layer);
-        itLy != out.layers.end())
+    if (auto itLy = out.layers.find(params_.target_layer); itLy != out.layers.end())
     {
         outPc = std::dynamic_pointer_cast<mrpt::maps::CPointsMap>(itLy->second);
         if (!outPc)
+        {
             THROW_EXCEPTION_FMT(
-                "Layer '%s' must be of point cloud type.",
-                params_.target_layer.c_str());
+                "Layer '%s' must be of point cloud type.", params_.target_layer.c_str());
+        }
     }
     else
     {
@@ -227,14 +215,13 @@ bool Generator::filterPointCloud(  //
         ASSERT_(outPc);
 
         MRPT_LOG_DEBUG_FMT(
-            "[filterPointCloud] Created new layer '%s' of type '%s'",
-            params_.target_layer.c_str(), outPc->GetRuntimeClass()->className);
+            "[filterPointCloud] Created new layer '%s' of type '%s'", params_.target_layer.c_str(),
+            outPc->GetRuntimeClass()->className);
 
         out.layers[params_.target_layer] = outPc;
     }
 
-    const mrpt::poses::CPose3D p =
-        robotPose ? robotPose.value() + sensorPose : sensorPose;
+    const mrpt::poses::CPose3D p = robotPose ? robotPose.value() + sensorPose : sensorPose;
 
     outPc->insertAnotherMap(&pc, p);
 
@@ -254,8 +241,7 @@ bool Generator::filterRotatingScan(  //
 
 bool mp2p_icp_filters::apply_generators(
     const GeneratorSet& generators, const mrpt::obs::CObservation& obs,
-    mp2p_icp::metric_map_t&                    output,
-    const std::optional<mrpt::poses::CPose3D>& robotPose)
+    mp2p_icp::metric_map_t& output, const std::optional<mrpt::poses::CPose3D>& robotPose)
 {
     ASSERT_(!generators.empty());
     bool anyHandled = false;
@@ -288,8 +274,7 @@ mp2p_icp::metric_map_t mp2p_icp_filters::apply_generators(
 
 bool mp2p_icp_filters::apply_generators(
     const GeneratorSet& generators, const mrpt::obs::CSensoryFrame& sf,
-    mp2p_icp::metric_map_t&                    output,
-    const std::optional<mrpt::poses::CPose3D>& robotPose)
+    mp2p_icp::metric_map_t& output, const std::optional<mrpt::poses::CPose3D>& robotPose)
 {
     ASSERT_(!generators.empty());
     bool anyHandled = false;
@@ -298,7 +283,10 @@ bool mp2p_icp_filters::apply_generators(
         ASSERT_(g.get() != nullptr);
         for (const auto& obs : sf)
         {
-            if (!obs) continue;
+            if (!obs)
+            {
+                continue;
+            }
             const bool handled = g->process(*obs, output, robotPose);
 
             anyHandled = anyHandled || handled;
@@ -310,7 +298,10 @@ bool mp2p_icp_filters::apply_generators(
 GeneratorSet mp2p_icp_filters::generators_from_yaml(
     const mrpt::containers::yaml& c, const mrpt::system::VerbosityLevel& vLevel)
 {
-    if (c.isNullNode()) return {};
+    if (c.isNullNode())
+    {
+        return {};
+    }
 
     ASSERT_(c.isSequence());
 
@@ -326,9 +317,7 @@ GeneratorSet mp2p_icp_filters::generators_from_yaml(
 
         auto f = std::dynamic_pointer_cast<Generator>(o);
         ASSERTMSG_(
-            f, mrpt::format(
-                   "`%s` class seems not to be derived from Generator",
-                   sClass.c_str()));
+            f, mrpt::format("`%s` class seems not to be derived from Generator", sClass.c_str()));
 
         f->setMinLoggingLevel(vLevel);
 
@@ -344,9 +333,7 @@ GeneratorSet mp2p_icp_filters::generators_from_yaml_file(
 {
     const auto yamlContent = mrpt::containers::yaml::FromFile(filename);
 
-    ASSERT_(
-        yamlContent.has("generators") &&
-        yamlContent["generators"].isSequence());
+    ASSERT_(yamlContent.has("generators") && yamlContent["generators"].isSequence());
 
     return generators_from_yaml(yamlContent["generators"], vLevel);
 }
@@ -369,7 +356,9 @@ bool Generator::implProcessDefault(
         !std::regex_match(obsClassName, process_class_names_regex_) ||
         !std::regex_match(o.sensorLabel, process_sensor_labels_regex_))
     {
-        MRPT_LOG_DEBUG_STREAM("Skipping this observation");
+        MRPT_LOG_DEBUG_STREAM(
+            "Skipping observation of type '" << obsClassName << "' with label '" << o.sensorLabel
+                                             << "'.");
         return false;
     }
 
@@ -377,19 +366,26 @@ bool Generator::implProcessDefault(
     o.load();
 
     if (auto oRS = dynamic_cast<const CObservationRotatingScan*>(&o); oRS)
+    {
         processed = filterRotatingScan(*oRS, out, robotPose);
+    }
     else if (auto o0 = dynamic_cast<const CObservationPointCloud*>(&o); o0)
     {
         ASSERT_(o0->pointcloud);
-        processed =
-            filterPointCloud(*o0->pointcloud, o0->sensorPose, out, robotPose);
+        processed = filterPointCloud(*o0->pointcloud, o0->sensorPose, out, robotPose);
     }
     else if (auto o1 = dynamic_cast<const CObservation2DRangeScan*>(&o); o1)
+    {
         processed = filterScan2D(*o1, out, robotPose);
+    }
     else if (auto o2 = dynamic_cast<const CObservation3DRangeScan*>(&o); o2)
+    {
         processed = filterScan3D(*o2, out, robotPose);
+    }
     else if (auto o3 = dynamic_cast<const CObservationVelodyneScan*>(&o); o3)
+    {
         processed = filterVelodyneScan(*o3, out, robotPose);
+    }
 
     // done?
     if (processed)
@@ -413,8 +409,7 @@ bool Generator::implProcessDefault(
 
     // Special case:
     // unproject 3D points, if needed:
-    if (auto obs3D =
-            dynamic_cast<const mrpt::obs::CObservation3DRangeScan*>(&o);
+    if (auto obs3D = dynamic_cast<const mrpt::obs::CObservation3DRangeScan*>(&o);
         obs3D && obs3D->points3D_x.empty())
     {
         mrpt::maps::CSimplePointsMap tmpMap;
@@ -422,28 +417,25 @@ bool Generator::implProcessDefault(
         mrpt::obs::T3DPointsProjectionParams pp;
         pp.takeIntoAccountSensorPoseOnRobot = true;
         pp.robotPoseInTheWorld              = robotPose;
-        const_cast<mrpt::obs::CObservation3DRangeScan*>(obs3D)->unprojectInto(
-            tmpMap, pp);
+        const_cast<mrpt::obs::CObservation3DRangeScan*>(obs3D)->unprojectInto(tmpMap, pp);
 
         outPc->insertAnotherMap(&tmpMap, mrpt::poses::CPose3D::Identity());
 
         return true;
     }
-    else
-    {
-        // General case:
-        const bool insertDone = o.insertObservationInto(*outPc, robotPose);
 
-        if (!insertDone && params_.throw_on_unhandled_observation_class)
-        {
-            THROW_EXCEPTION_FMT(
-                "Observation of type '%s' could not be converted into a "
-                "point cloud, and none of the specializations handled it, "
-                "so I do not know what to do with this observation!",
-                obsClassName);
-        }
-        return insertDone;
+    // General case:
+    const bool insertDone = o.insertObservationInto(*outPc, robotPose);
+
+    if (!insertDone && params_.throw_on_unhandled_observation_class)
+    {
+        THROW_EXCEPTION_FMT(
+            "Observation of type '%s' could not be converted into a "
+            "point cloud, and none of the specializations handled it, "
+            "so I do not know what to do with this observation!",
+            obsClassName);
     }
+    return insertDone;
 
     // o.unload();  // DON'T! We don't know who else is using the data
 }
@@ -459,8 +451,7 @@ bool Generator::implProcessCustomMap(
     // Create if new: Append to existing layer, if already existed.
     mrpt::maps::CMetricMap::Ptr outMap;
 
-    if (auto itLy = out.layers.find(params_.target_layer);
-        itLy != out.layers.end())
+    if (auto itLy = out.layers.find(params_.target_layer); itLy != out.layers.end())
     {
         outMap = itLy->second;
     }
@@ -477,8 +468,7 @@ bool Generator::implProcessCustomMap(
             // Load from INI file
             // ------------------------------
             ASSERT_FILE_EXISTS_(params_.metric_map_definition_ini_file);
-            mrpt::config::CConfigFile cfg(
-                params_.metric_map_definition_ini_file);
+            mrpt::config::CConfigFile cfg(params_.metric_map_definition_ini_file);
             mapInits.loadFromConfigFile(cfg, cfgPrefix);
         }
         else
@@ -500,31 +490,30 @@ bool Generator::implProcessCustomMap(
             if (c.has("plugin"))
             {
                 const auto moduleToLoad = c["plugin"].as<std::string>();
-                MRPT_LOG_DEBUG_STREAM(
-                    "About to load user-defined plugin: " << moduleToLoad);
+                MRPT_LOG_DEBUG_STREAM("About to load user-defined plugin: " << moduleToLoad);
                 internalLoadUserPlugin(moduleToLoad);
             }
 
             // fill the rest sub-sections:
             for (const auto& [k, v] : c.asMap())
             {
-                if (!v.isMap()) continue;
-                const auto keyVal = k.as<std::string>();
-                const auto sectName =
-                    cfgPrefix + "_"s + mapClass + "_00_"s + keyVal;
+                if (!v.isMap())
+                {
+                    continue;
+                }
+                const auto keyVal   = k.as<std::string>();
+                const auto sectName = cfgPrefix + "_"s + mapClass + "_00_"s + keyVal;
                 for (const auto& [kk, vv] : v.asMap())
                 {
                     ASSERT_(kk.isScalar());
                     ASSERT_(vv.isScalar());
 
-                    cfg.write(
-                        sectName, kk.as<std::string>(), vv.as<std::string>());
+                    cfg.write(sectName, kk.as<std::string>(), vv.as<std::string>());
                 }
             }
 
             MRPT_LOG_DEBUG_STREAM(
-                "Built INI-like block for layer '" << params_.target_layer
-                                                   << "':\n"
+                "Built INI-like block for layer '" << params_.target_layer << "':\n"
                                                    << cfg.getContent());
 
             // parse it:
@@ -548,7 +537,9 @@ bool Generator::implProcessCustomMap(
         !std::regex_match(obsClassName, process_class_names_regex_) ||
         !std::regex_match(o.sensorLabel, process_sensor_labels_regex_))
     {
-        MRPT_LOG_DEBUG_STREAM("Skipping this observation");
+        MRPT_LOG_DEBUG_STREAM(
+            "Skipping observation of type '" << obsClassName << "' with label '" << o.sensorLabel
+                                             << "'.");
         return false;
     }
 
@@ -575,7 +566,10 @@ namespace
 {
 void safe_add_to_list(const std::string& path, std::vector<std::string>& lst)
 {
-    if (mrpt::system::directoryExists(path)) lst.push_back(path);
+    if (mrpt::system::directoryExists(path))
+    {
+        lst.push_back(path);
+    }
 }
 
 void from_env_var_to_list(
@@ -588,16 +582,17 @@ void from_env_var_to_list(
     const auto delim  = std::string(":");
 #endif
 
-    const auto additionalPaths = mrpt::get_env<std::string>(env_var_name);
+    const auto               additionalPaths = mrpt::get_env<std::string>(env_var_name);
     std::vector<std::string> pathList;
     mrpt::system::tokenize(additionalPaths, delim, pathList);
 
     // Append to list:
     for (const auto& path : pathList)
     {
-        if (!subStringPattern.empty() &&
-            path.find(subStringPattern) == std::string::npos)
+        if (!subStringPattern.empty() && path.find(subStringPattern) == std::string::npos)
+        {
             continue;
+        }
         safe_add_to_list(path, lst);
     }
 }
@@ -653,10 +648,12 @@ void Generator::internalLoadUserPlugin(const std::string& moduleToLoad) const
     if (handle == nullptr)
     {
         const char* err = dlerror();
-        if (!err) err = "(error calling dlerror())";
-        THROW_EXCEPTION(mrpt::format(
-            "Error loading module: `%s`\ndlerror(): `%s`", absPath.c_str(),
-            err));
+        if (!err)
+        {
+            err = "(error calling dlerror())";
+        }
+        THROW_EXCEPTION(
+            mrpt::format("Error loading module: `%s`\ndlerror(): `%s`", absPath.c_str(), err));
     }
 
     MRPT_LOG_DEBUG_FMT("Successfully loaded: `%s`", absPath.c_str());
--- mp2p_icp_map/src/estimate_points_eigen.cpp
+++ mp2p_icp_map/src/estimate_points_eigen.cpp
@@ -18,8 +18,7 @@
 
 mp2p_icp::PointCloudEigen mp2p_icp::estimate_points_eigen(
     const float* xs, const float* ys, const float* zs,
-    mrpt::optional_ref<const std::vector<size_t>> indices,
-    std::optional<size_t>                         totalCount)
+    mrpt::optional_ref<const std::vector<size_t>> indices, std::optional<size_t> totalCount)
 {
     MRPT_START
 
@@ -31,9 +30,11 @@ mp2p_icp::PointCloudEigen mp2p_icp::estimate_points_eigen(
     if (totalCount)
     {
         if (*totalCount < 3)
+        {
             THROW_EXCEPTION("totalCount: at least 3 points required.");
+        }
 
-        const float inv_n = (1.0f / *totalCount);
+        const float inv_n = 1.0f / static_cast<float>(*totalCount);
         for (size_t i = 0; i < *totalCount; i++)
         {
             mean.x += xs[i];
@@ -59,9 +60,11 @@ mp2p_icp::PointCloudEigen mp2p_icp::estimate_points_eigen(
         const auto& idxs = indices->get();
 
         if (idxs.size() < 3)
+        {
             THROW_EXCEPTION("indices: at least 3 points required.");
+        }
 
-        const float inv_n = (1.0f / idxs.size());
+        const float inv_n = 1.0f / static_cast<float>(idxs.size());
         for (size_t i = 0; i < idxs.size(); i++)
         {
             const auto pt_idx = idxs[i];
@@ -73,9 +76,7 @@ mp2p_icp::PointCloudEigen mp2p_icp::estimate_points_eigen(
         for (size_t i = 0; i < idxs.size(); i++)
         {
             const auto pt_idx = idxs[i];
-            const auto a =
-                mrpt::math::TPoint3Df(xs[pt_idx], ys[pt_idx], zs[pt_idx]) -
-                mean;
+            const auto a      = mrpt::math::TPoint3Df(xs[pt_idx], ys[pt_idx], zs[pt_idx]) - mean;
             mat_a(0, 0) += a.x * a.x;
             mat_a(1, 0) += a.x * a.y;
             mat_a(2, 0) += a.x * a.z;
@@ -114,8 +115,8 @@ mp2p_icp::PointCloudEigen mp2p_icp::estimate_points_eigen(
 }
 
 void mp2p_icp::vector_of_points_to_xyz(
-    const std::vector<mrpt::math::TPoint3Df>& pts, std::vector<float>& xs,
-    std::vector<float>& ys, std::vector<float>& zs)
+    const std::vector<mrpt::math::TPoint3Df>& pts, std::vector<float>& xs, std::vector<float>& ys,
+    std::vector<float>& zs)
 {
     const auto N = pts.size();
     xs.resize(N);
